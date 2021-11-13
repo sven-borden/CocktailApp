a9772a66850168b04ac339e2f9626a30539869ac
Sven <glaglasven@live.com>
2021-11-13 10:21:40 +0100

    Restarted the app

---
+++ New/CocktailApp/Shared/CocktailAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailAppApp.swift
+//  Shared
+//
+//  Created by Sven Borden 2 on 13.11.21.
+//
+
+import SwiftUI
+
+@main
+struct CocktailAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
